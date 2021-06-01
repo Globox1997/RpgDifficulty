82ae1cd26170276217d489f4ca295819cfb657a0
Globox1997 <mitschke11@web.de>
2021-06-01 11:14:16 +0200

    crash fix

--- src/main/java/net/rpgdifficulty/mixin/MooshroomEntityMixin.java
+++ src/main/java/net/rpgdifficulty/mixin/MooshroomEntityMixin.java
@@ -6,25 +6,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.asm.mixin.injection.At;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.passive.MooshroomEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.world.World;
 import net.rpgdifficulty.api.MobStrengthener;
 
 @Mixin(MooshroomEntity.class)
-public abstract class MooshroomEntityMixin extends CowEntity {
-
-    public MooshroomEntityMixin(EntityType<? extends CowEntity> entityType, World world) {
-        super(entityType, world);
-    }
+public class MooshroomEntityMixin {
 
     @Inject(method = "sheared", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/CowEntity;refreshPositionAndAngles(DDDFF)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void shearedMixin(SoundCategory shearedSoundCategory, CallbackInfo info, CowEntity cowEntity) {
-        if (world instanceof ServerWorld) {
-            MobStrengthener.changeAttributes(cowEntity, (ServerWorld) world);
+        if (cowEntity.world instanceof ServerWorld) {
+            MobStrengthener.changeAttributes(cowEntity, (ServerWorld) cowEntity.world);
         }
     }
 }
