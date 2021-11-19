96f41c9047e90f8fdb86d16faa40ed06415278c9
Globox1997 <mitschke11@web.de>
2021-11-19 11:15:08 +0100

    update to mc 1.18

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 RpgDifficulty is a mod which strengthens mobs over time and distance.
 
 ### Installation
-RpgDifficulty is a mod built for the [Fabric Loader](https://fabricmc.net/). It requires [Fabric API](https://www.curseforge.com/minecraft/mc-mods/fabric-api) to be installed separately; all other dependencies are installed with the mod.
+RpgDifficulty is a mod built for the [Fabric Loader](https://fabricmc.net/). It requires [Fabric API](https://www.curseforge.com/minecraft/mc-mods/fabric-api) and [Cloth Config API](https://www.curseforge.com/minecraft/mc-mods/cloth-config) to be installed separately; all other dependencies are installed with the mod.
 
 ### License
 RpgDifficulty is licensed under MIT.
--- src/main/java/net/rpgdifficulty/mixin/WitherSkullBlockMixin.java
+++ src/main/java/net/rpgdifficulty/mixin/WitherSkullBlockMixin.java
@@ -19,9 +19,9 @@ import net.rpgdifficulty.api.MobStrengthener;
 public class WitherSkullBlockMixin {
 
     @Inject(method = "Lnet/minecraft/block/WitherSkullBlock;onPlaced(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/SkullBlockEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/boss/WitherEntity;refreshPositionAndAngles(DDDFF)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private static void onPlacedMixin(World world, BlockPos pos, SkullBlockEntity blockEntity, CallbackInfo info, BlockPattern blockPattern, BlockPattern.Result result, WitherEntity witherEntity) {
+    private static void onPlacedMixin(World world, BlockPos pos, SkullBlockEntity blockEntity, CallbackInfo info, boolean bl, BlockPattern blockPattern, BlockPattern.Result result, WitherEntity i) {
         if (world instanceof ServerWorld) {
-            MobStrengthener.changeBossAttributes(witherEntity, (ServerWorld) world);
+            MobStrengthener.changeBossAttributes(i, (ServerWorld) world);
         }
     }
 }
