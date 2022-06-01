e2ddea6a8d66fd1fe438060c508f419c3e60b7a4
Globox1997 <mitschke11@web.de>
2022-06-01 17:38:19 +0200

    added config clock

---
+++ src/main/java/net/rpgdifficulty/config/ConfigClock.java
@@ -0,0 +1,52 @@
+package net.rpgdifficulty.config;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+
+public class ConfigClock extends DrawableHelper implements Drawable {
+
+    private final Identifier CLOCK_TEXTURE = new Identifier("rpgdifficulty", "textures/gui/clock.png");
+    private final TranslatableText translatableText;
+    private final MinecraftClient minecraftClient;
+    private int x;
+    private int y;
+
+    public ConfigClock(MinecraftClient client, int x, int y) {
+        this.minecraftClient = client;
+        this.x = x;
+        this.y = y;
+        translatableText = new TranslatableText("text.autoconfig.rpgdifficulty.clock", client.world.getTime() / 60);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float var4) {
+        RenderSystem.setShaderTexture(0, CLOCK_TEXTURE);
+        DrawableHelper.drawTexture(matrices, this.x, this.y, 0, 0, 16, 16, 16, 16);
+
+        if (isMouseWithinBounds(16, 16, mouseX, mouseY))
+            renderMousehoverTooltip(matrices, mouseX, mouseY);
+    }
+
+    private boolean isMouseWithinBounds(int width, int height, double pointX, double pointY) {
+        return pointX >= x && pointX <= x + width && pointY >= y && pointY <= y + height;
+    }
+
+    private void renderMousehoverTooltip(MatrixStack matrices, int mouseX, int mouseY) {
+        int j = minecraftClient.textRenderer.getWidth(translatableText);
+        int l = mouseX - j - 5;
+        int m = mouseY;
+        if (l < 0)
+            l = mouseX + 12;
+
+        int n = m - 3;
+        this.fillGradient(matrices, l - 3, n, l + j + 3, m + 8 + 3, -1073741824, -1073741824);
+        minecraftClient.textRenderer.drawWithShadow(matrices, translatableText, (float) l, (float) m, 0xFFFFFF);
+    }
+
+}
--- src/main/java/net/rpgdifficulty/config/ModMenuIntegration.java
+++ src/main/java/net/rpgdifficulty/config/ModMenuIntegration.java
@@ -3,14 +3,33 @@ package net.rpgdifficulty.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.gui.ConfigScreenProvider;
+import me.shedaniel.cloth.api.client.events.v0.ScreenHooks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
+    @SuppressWarnings("deprecation")
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(RpgDifficultyConfig.class, parent).get();
+        return parent -> {
+            ConfigScreenProvider<RpgDifficultyConfig> supplier = (ConfigScreenProvider<RpgDifficultyConfig>) AutoConfig.getConfigScreen(RpgDifficultyConfig.class, parent);
+            supplier.setBuildFunction(builder -> {
+                builder.setAfterInitConsumer(screen -> {
+                    MinecraftClient minecraftClient = MinecraftClient.getInstance();
+                    if (minecraftClient.world != null)
+                        ((ScreenHooks) screen).cloth$addDrawable(new ConfigClock(minecraftClient, screen.width - 35, 14));
+
+                    // ((ScreenHooks) screen).cloth$addDrawableChild(new ButtonWidget(screen.width - 104, 4, 100, 20, new TranslatableText("text.moderate-loading-screen.testButton"), button -> {
+                    // Execute something
+                    // }));
+                });
+                return builder.build();
+            });
+            return supplier.get();
+        };
     }
 }
