dbedb4509acdf1eb68921f44b978451292e448a6
Globox1997 <mitschke11@web.de>
2022-06-22 18:16:42 +0200

    added c bosses tag

--- src/main/java/net/rpgdifficulty/RpgDifficultyMain.java
+++ src/main/java/net/rpgdifficulty/RpgDifficultyMain.java
@@ -4,12 +4,19 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.EntityType;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.rpgdifficulty.config.RpgDifficultyConfig;
 
 public class RpgDifficultyMain implements ModInitializer {
 
     public static RpgDifficultyConfig CONFIG = new RpgDifficultyConfig();
+
     public static final boolean isNameplateLoaded = FabricLoader.getInstance().isModLoaded("nameplate");
+
+    public static final TagKey<EntityType<?>> BOSS_ENTITY_TYPES = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier("c", "bosses"));
 
     @Override
     public void onInitialize() {
