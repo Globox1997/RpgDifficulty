2566f5c290bace2565c496592bce8e6860423292
Globox1997 <mitschke11@web.de>
2022-08-22 13:31:00 +0200

    added rpgz compat

--- src/main/java/net/rpgdifficulty/mixin/compat/MoreLootDropMixin.java
+++ src/main/java/net/rpgdifficulty/mixin/compat/MoreLootDropMixin.java
@@ -8,20 +8,14 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.rpgdifficulty.api.MobStrengthener;
+import net.rpgz.access.InventoryAccess;
 
 @Mixin(MobStrengthener.class)
 public class MoreLootDropMixin {
 
-    // public static void dropMoreLoot(LivingEntity livingEntity, LootTable lootTable, LootContext.Builder builder) {
-
-    // }
-
     @Redirect(method = "dropMoreLoot", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;dropStack(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/ItemEntity;"))
     private static ItemEntity dropMoreLootMixin(LivingEntity livingEntity, ItemStack itemStack) {
-
-        System.out.println(itemStack);
+        ((InventoryAccess) livingEntity).addingInventoryItems(itemStack);
         return null;
-        // (Lnet/minecraft/entity/ItemEntity;)Lnet/minecraft/entity/ItemEntity; Expected signature:
-        // (Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/ItemEntity;
     }
 }
