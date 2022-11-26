82671023497cb9c1c83c8e672414e90d8a9a3831
Globox1997 <mitschke11@web.de>
2022-11-26 18:36:17 +0100

    moved mixin

---
+++ src/main/java/net/rpgdifficulty/mixin/client/ZombieEntityRendererMixin.java
@@ -0,0 +1,31 @@
+package net.rpgdifficulty.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.ZombieBaseEntityRenderer;
+import net.minecraft.client.render.entity.ZombieEntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.render.entity.model.ZombieEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.rpgdifficulty.RpgDifficultyMain;
+import net.rpgdifficulty.access.EntityAccess;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ZombieEntityRenderer.class)
+public abstract class ZombieEntityRendererMixin extends ZombieBaseEntityRenderer<ZombieEntity, ZombieEntityModel<ZombieEntity>> {
+
+    public ZombieEntityRendererMixin(Context ctx, ZombieEntityModel<ZombieEntity> bodyModel, ZombieEntityModel<ZombieEntity> legsArmorModel, ZombieEntityModel<ZombieEntity> bodyArmorModel) {
+        super(ctx, bodyModel, legsArmorModel, bodyArmorModel);
+    }
+
+    @Override
+    protected void scale(ZombieEntity entity, MatrixStack matrices, float amount) {
+        if (entity.getDataTracker().get(((EntityAccess) entity).getTrackedDataBoolean()))
+            matrices.scale(RpgDifficultyMain.CONFIG.bigZombieSize, RpgDifficultyMain.CONFIG.bigZombieSize, RpgDifficultyMain.CONFIG.bigZombieSize);
+        super.scale(entity, matrices, amount);
+    }
+
+}
