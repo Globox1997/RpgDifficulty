2810bcfd232bdd35d49d2b45b4717d6f2ffd41db
Globox1997 <mitschke11@web.de>
2023-09-01 09:36:30 +0200

    flush

--- src/main/java/net/rpgdifficulty/mixin/access/DefaultAttributeRegistryAccess.java
+++ src/main/java/net/rpgdifficulty/mixin/access/DefaultAttributeRegistryAccess.java
@@ -2,12 +2,15 @@ package net.rpgdifficulty.mixin.access;
 
 import java.util.Map;
 
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.DefaultAttributeRegistry;
 
+@Mixin(DefaultAttributeRegistry.class)
 public interface DefaultAttributeRegistryAccess {
 
     @Accessor("DEFAULT_ATTRIBUTE_REGISTRY")
