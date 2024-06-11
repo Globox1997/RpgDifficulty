9a50c4ffc252e33647f032f0200924c76403ecac
Globox1997 <mitschke11@web.de>
2024-06-11 21:21:05 +0200

    flush

--- src/main/java/net/rpgdifficulty/mixin/entity/ZombieEntityMixin.java
+++ src/main/java/net/rpgdifficulty/mixin/entity/ZombieEntityMixin.java
@@ -1,6 +1,7 @@
 package net.rpgdifficulty.mixin.entity;
 
 import net.minecraft.nbt.NbtCompound;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
